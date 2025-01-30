9a4f306aa9340a819c393ee41384500c8a14bdd8
JmRubi0 <johnmatthewrubio0530@gmail.com>
2025-01-30 13:26:00 -0500

    first commit

---
+++ demo/src/main/java/ca/vanier/budgetmanagement/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package ca.vanier.budgetmanagement.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	//this is a test
+
+}
